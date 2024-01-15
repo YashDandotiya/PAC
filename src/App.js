4523147eed6362bb1f947282b4bb36626dd91459
YashDandotiya <dandotiya.yash73@gmail.com>
2024-01-15 23:29:02 +0530

    pac site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import WhatWeDo from './pages/Whatwedo';
+import Pastevent from './past-event/Pastevent';
+import Team from './team-page/Team';
+import Announce from './announcements/Announce';
+import OurHistory from './ourhistory/Ourhistory';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <WhatWeDo/>
+   <Pastevent/>
+   <Announce/>
+   <OurHistory/>
+   </>
   );
 }
 
